fb764b01ca75a1b517193f223c571ed41b3dac61
luke <l.j.dwsn@gmail.com>
2019-11-04 20:29:33 +1100

    upload before test

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,10 +4,10 @@ class Profile < ApplicationRecord
   has_one_attached :picture
 
   
-  # validates :name, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }  
-  # validates :phone_number, numericality: { :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
-  # :message => "- Phone numbers must be in xxx-xxx-xxxx format." } 
-  
+  validates :name, presence: true, length: { minimum: 1, maximum: 20 , message: "Display name must be between: 20 - 500 characters" }
+  validates :phone_number, numericality: { :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
+  :message => "- Phone numbers must be in xxx-xxx-xxxx format." }
+  validates :address, presence: true
 
 #   def completed_profile?
 #     self.name.present? && self.address.present? && self.phone_number.present? && self.email.present? 
--- app/models/videogame.rb
+++ app/models/videogame.rb
@@ -5,18 +5,17 @@ class Videogame < ApplicationRecord
   belongs_to :genre 
   belongs_to :developer 
  
-  validates :name, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
-  
+  validates :name, presence: true, length: { minimum: 1, maximum: 20 , message: "Name must be between: 20 - 500 characters" }
   # validates :release_date, format: { with: /\d{2}\/\d{2}\/\d{4}/, message: "Date must be in the following format: dd/mm/yyyy" }
 
-  validates :average_rating, inclusion:  {:in => [1,5], message: "Score must between: 1-5" }
+  validates :average_rating, inclusion:  {:in => (1..5), message: "Score must between: 1-5" }
   
   validates :genre_id, presence: true
   validates :developer_id, presence: true
   
-  validates :description, presence: true, length: { minimum: 20, maximum: 300 , message: "Description must be between: 20 - 500 words" }
+  validates :description, presence: true, length: { minimum: 20, maximum: 300 , message: "Description must be between: 20 - 500 characters" }
 
-  validates :system_req, inclusion:  {:in => ["low","medium", "high"], message: "System requirements can only be low, medium or high" }
+  validates :system_req, inclusion:  {:in => %w(low medium high), message: "System requirements can only be Low, Medium or High" }
 
   
 
