9b09d9f6fd5c782a913ec9fe2b3426f46db8d23a
luke <l.j.dwsn@gmail.com>
2019-11-08 14:31:14 +1100

    seeding changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,25 +8,25 @@
 require 'faker'
 
     
-for i in 1..50
-  user = User.create!(
+# for i in 1..50
+#   user = User.create!(
      
-    email: Faker::Internet.email ,
-    password: "password" 
-  ) 
-  profile = Profile.new( 
-    name: Faker::GreekPhilosophers.name ,
-    country: Faker::Address.country ,
-    phone_number: Faker::Number.number(digits: 10)    ,
-    email: user.email 
+#     email: Faker::Internet.email ,
+#     password: "password" 
+#   ) 
+#   profile = Profile.new( 
+#     name: Faker::GreekPhilosophers.name ,
+#     country: Faker::Address.country ,
+#     phone_number: Faker::Number.number(digits: 10)    ,
+#     email: user.email 
 
-    )
+#     )
      
   
-  profile.user = user
-  profile.save
-  puts "Created #{i} Users #{user.id} and Profile #{profile.id} "
-end 
+#   profile.user = user
+#   profile.save
+#   puts "Created #{i} Users #{user.id} and Profile #{profile.id} "
+# end 
 
 
 for i in 0..50
@@ -45,35 +45,35 @@ for i in 0..25
 end
 
   
-spec = ["Low", "Medium", "High"]
-number = [1,2,3,4,5]
+# spec = ["Low", "Medium", "High"]
+# number = [1,2,3,4,5]
 
   
-  for i in 1..50
-    Videogame.create!(
-     name: Faker::Game.title ,
-     developer_id: Developer.find(rand(1..50)).id,
-     description: Faker::Quote.matz,
-     system_req: spec.sample, 
-     release_date: Faker::Date.between_except(from: 30.year.ago, to: 1.year.from_now,  excepted: Date.today) ,
-     average_rating: number.sample ,
-     genre_id: Genre.find(rand(1..25)).id,
-     )
+  # for i in 1..50
+  #   Videogame.create!(
+  #    name: Faker::Game.title ,
+  #    developer_id: Developer.find(rand(1..50)).id,
+  #    description: Faker::Quote.matz,
+  #    system_req: spec.sample, 
+  #    release_date: Faker::Date.between_except(from: 30.year.ago, to: 1.year.from_now,  excepted: Date.today) ,
+  #    average_rating: number.sample ,
+  #    genre_id: Genre.find(rand(1..25)).id,
+  #    )
      
-  end
+  # end
 
-    status = ["uploader", "dowloader"]
+  #   status = ["uploader", "dowloader"]
 
-  for i in 1..50 
-     ProfileVideogame.find_or_create_by!(
-       videogame_id: Videogame.find(rand(1..50)).id, 
-       profile_id: Profile.find(rand(1..50)).id ,
-       role: status.sample ,
-       )
+  # for i in 1..50 
+  #    ProfileVideogame.find_or_create_by!(
+  #      videogame_id: Videogame.find(rand(1..50)).id, 
+  #      profile_id: Profile.find(rand(1..50)).id ,
+  #      role: status.sample ,
+  #      )
 
      
      
-   end
+  #  end
 
   puts "Seeding complete"
 
